7515b532571553c177c721ed1dfec80e0e2501c0
Nguyen-Mihn <109030820+Nguyen-Mihn@users.noreply.github.com>
2022-08-01 20:38:06 +0800

    My VN-EN Dictionary

---
+++ MyDict/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace MyDict
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.definitionBox = new System.Windows.Forms.RichTextBox();
+            this.wordBox = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.reverseButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // definitionBox
+            // 
+            this.definitionBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.definitionBox.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.definitionBox.ForeColor = System.Drawing.Color.White;
+            this.definitionBox.Location = new System.Drawing.Point(19, 26);
+            this.definitionBox.Name = "definitionBox";
+            this.definitionBox.Size = new System.Drawing.Size(663, 313);
+            this.definitionBox.TabIndex = 0;
+            this.definitionBox.Text = "";
+            // 
+            // wordBox
+            // 
+            this.wordBox.Location = new System.Drawing.Point(19, 354);
+            this.wordBox.Name = "wordBox";
+            this.wordBox.Size = new System.Drawing.Size(558, 25);
+            this.wordBox.TabIndex = 1;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(125, 399);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(83, 27);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // reverseButton
+            // 
+            this.reverseButton.Location = new System.Drawing.Point(243, 400);
+            this.reverseButton.Name = "reverseButton";
+            this.reverseButton.Size = new System.Drawing.Size(80, 26);
+            this.reverseButton.TabIndex = 3;
+            this.reverseButton.Text = "Reverse";
+            this.reverseButton.UseVisualStyleBackColor = true;
+            this.reverseButton.Click += new System.EventHandler(this.reverseButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(364, 399);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(95, 26);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(489, 399);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(86, 25);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(19, 400);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(79, 26);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.reverseButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.wordBox);
+            this.Controls.Add(this.definitionBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox definitionBox;
+        private System.Windows.Forms.TextBox wordBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button reverseButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ MyDict/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDict
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://123.56.139.184/tudien/search.php" + "?tu=" + wordBox.Text.ToString();
+            getBase(url);
+        }
+        private void getBase(string url)
+        {
+            //httpGet(url);
+            try
+            {
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                WebProxy proxy = WebProxy.GetDefaultProxy();
+                if (proxy.Address != null && proxy.Address.AbsoluteUri != string.Empty)
+                {
+                    request.Proxy = GlobalProxySelection.GetEmptyWebProxy();
+                }
+                request.Method = "GET";
+                request.KeepAlive = false;
+                request.Timeout = 10000;
+                //解决远程连接不可用（503）问题
+                request.Headers.Set("Pragma", "no-cache");
+                //request.UserAgent = "Mozilla-Firefox-Spider(Wenanry)";
+                request.UserAgent = "Little Verifier Process";
+                var webResponse = request.GetResponse();
+                var webStream = webResponse.GetResponseStream();
+                var reader = new StreamReader(webStream);
+                var data = reader.ReadToEnd();
+                data = data.Replace("\n", "\n\t");
+                definitionBox.Text = "";
+                displayText("Result: \n\t", Color.Red);
+                if (data.Contains('\n'))
+                {
+                    String[] items = data.Split('\n');
+                    foreach (String item in items)
+                    {
+                        if (item.Contains(':'))
+                        {
+                            String[] result = item.Split(':');
+                            displayText(result[0], Color.FromArgb(255, 255, 185, 0));
+                            displayText(": ", Color.White);
+                            displayText(result[1] + "\n", Color.FromArgb(255, 127, 186, 0));
+
+                        }
+                    }
+                }
+                else
+                {
+                    displayText(wordBox.Text, Color.FromArgb(255, 255, 185, 0));
+                    displayText(": ", Color.White);
+                    displayText(data + "\n", Color.FromArgb(255, 127, 186, 0));
+                }
+                
+                
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        private void displayText(string text, Color color)
+        {
+            definitionBox.SelectionStart = definitionBox.TextLength;
+            definitionBox.SelectionLength = 0;
+            definitionBox.SelectionColor = color;
+            definitionBox.AppendText(text);
+        }
+        static string r;
+        static async Task httpGet(string url)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+            r = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(r); //
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://123.56.139.184/tudien/add.php" + "?tu=" + wordBox.Text.ToString() + "&nghia=" + definitionBox.Text;
+            getBase(url);
+        }
+
+        private void reverseButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://123.56.139.184/tudien/reverse.php?nghia=" + wordBox.Text;
+            getBase(url);
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            string url = "http://123.56.139.184/tudien/update.php?tu=" + wordBox.Text + "&nghia=" + definitionBox.Text;
+            getBase(url);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
